a8ac7fb77b5e5ed116f75b34df773ed5a731348b
Nton118 <114150459+Nton118@users.noreply.github.com>
2023-07-23 18:55:56 +0300

    3

--- quotes_site/quotes/forms.py
+++ quotes_site/quotes/forms.py
@@ -1,11 +1,22 @@
-from django.forms import ModelForm, CharField, TextInput, ModelMultipleChoiceField, SelectMultiple, Textarea
+from django.forms import ModelForm, CharField, TextInput, ModelMultipleChoiceField, SelectMultiple, Textarea, ValidationError
 from .models import Author, Quote, Tag
+from django.shortcuts import redirect, reverse
 
 
 class QuoteForm(ModelForm):
-    quote = CharField(min_length=5, required=True, widget=TextInput(attrs={'class': 'form-control'}))
-    tags = ModelMultipleChoiceField(required=True, queryset=Tag.objects.all(), widget=SelectMultiple)
     author = CharField(min_length=5, max_length=150, required=True, widget=TextInput(attrs={'class': 'form-control'}))
+    quote = CharField(min_length=5, required=True, widget=Textarea(attrs={'class': 'form-control'}))
+    tags = ModelMultipleChoiceField(required=True, queryset=Tag.objects.all(),
+                                    widget=SelectMultiple(attrs={'class': 'form-control'}))
+
+    def clean_author(self):
+        author_name = self.cleaned_data.get('author')
+        # Перевіряємо, чи автор з таким іменем вже існує в базі даних
+        if not Author.objects.filter(fullname=author_name).exists():
+            # Якщо автор не існує, перенаправляємо користувача на сторінку вводу нового автора
+            return redirect(reverse('create_author'))
+
+        return author_name
 
     class Meta:
         model = Quote
@@ -16,7 +27,13 @@ class AuthorForm(ModelForm):
     fullname = CharField(max_length=40, min_length=5, widget=TextInput(attrs={'class': 'form-control'}))
     born_date = CharField(max_length=25, min_length=12, widget=TextInput(attrs={'class': 'form-control'}))
     born_location = CharField(max_length=150, min_length=12, widget=TextInput(attrs={'class': 'form-control'}))
-    description = CharField(widget=Textarea)
+    description = CharField(widget=Textarea(attrs={'class': 'form-control'}))
+
+    def clean_fullname(self):
+        fullname = self.cleaned_data.get('fullname')
+        if Author.objects.filter(fullname=fullname).exists():
+            raise ValidationError("Such author already exists in the database")
+        return fullname
 
     class Meta:
         model = Author
@@ -24,6 +41,12 @@ class AuthorForm(ModelForm):
 
 class TagForm(ModelForm):
     name = CharField(max_length=40, min_length=2, widget=TextInput(attrs={'class': 'form-control'}))
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if Tag.objects.filter(name=name).exists():
+            raise ValidationError("Such Tag already exists in the database")
+        return name
 
     class Meta:
         model = Tag
--- quotes_site/quotes/views.py
+++ quotes_site/quotes/views.py
@@ -27,15 +27,14 @@ def index(request, page=1):
 
 
 def author(request, aut_name):
-    name = aut_name.replace('%20',' ')
-    author_details = Author.objects.get(fullname=name)
+    author_details = Author.objects.get(fullname=aut_name)
     return render(request, 'quotes/author.html', context={'author': author_details})
 
 
 def tag(request, tag_name, page=1):
     tag_id=Tag.objects.get(name=tag_name)
     quotes = Quote.objects.filter(tags=tag_id)
-    per_page = 10
+    per_page = 5
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
     top_ten_tags = find_top_ten_tags()
@@ -45,7 +44,20 @@ def tag(request, tag_name, page=1):
 
 @login_required
 def add_quote(request):
-    ...
+    form = QuoteForm(instance=Quote())
+    if request.method == "POST":
+        form = QuoteForm(request.POST, instance=Quote())
+        if form.is_valid():
+            quote = form.save(commit=False)
+            quote.quote = request.quote
+            quote.tags = request.tags
+            quote.author = request.author
+            quote.save()
+            if quote.author not in Author.objects.fullname:
+                return redirect(to='quotes:add_author', context={'title': 'add new author!',
+                                                                 'author': quote.author.fullname})
+            return redirect(to='quotes:main')
+    return render(request, 'quotes/add_quote.html', context={'title': 'Add Quote', 'form': form})
 
 
 @login_required
@@ -55,16 +67,29 @@ def del_quote(request):
 
 @login_required
 def add_author(request):
-    ...
+    form = AuthorForm(instance=Author())
+    if request.method == "POST":
+        form = AuthorForm(request.POST, instance=Author())
+        if form.is_valid():
+            author = form.save(commit=False)
+            author.fullname = request.fullname
+            author.born_date = request.born_date
+            author.born_location = request.born_location
+            author.save()
+            return redirect(to='quotes:main')
+    return render(request, 'quotes/add_author.html', context={'title': 'Add Author', 'form': form})
+
 
 
 @login_required
 def del_author(request):
     ...
 
+
 @login_required
 def add_tag(request):
     ...
+
 
 @login_required
 def del_tag(request):
